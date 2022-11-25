47f2b0c9eedaf31dcfd1bf08dcf65bf72e8d01d8
devkeyur <keyurmalete1554@gmail.com>
2022-11-25 16:58:21 +0530

    K-shoppe
    Online Shopping website

---
+++ script.js
@@ -0,0 +1,18 @@
+var dt = new Date().toLocaleTimeString();
+document.getElementById("time").innerHTML=dt
+
+const bar = document.getElementById('bar');
+const close = document.getElementById('close');
+const nav = document.getElementById('navbar');
+
+if (bar) {
+    bar.addEventListener('click',()=>{
+        nav.classList.add('active')
+    })
+}
+
+if (close) {
+    close.addEventListener('click',()=>{
+        nav.classList.remove('active')
+    })
+}
